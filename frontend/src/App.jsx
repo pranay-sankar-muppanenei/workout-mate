a83e615b60afcd7a539a2fb4afacf5b54c9d4fa5
pranay341 <ro200341@rguktong.ac.in>
2025-10-06 21:40:48 +0530

    not found

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Route path='/' element={user?<Home/>:<Navigate to='/login'/>}/>
     <Route path='/login' element={!user?<Login/>:<Navigate to='/'/>}/>
     <Route path='/signup' element={!user?<Signup/>:<Navigate to='/'/>}/> 
-    <Route element={<NotFound/>}/>
+    <Route path='*' element={<NotFound/>}/>
   </Routes>
 </BrowserRouter>
   )
