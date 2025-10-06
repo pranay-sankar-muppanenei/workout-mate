8c37e9f10e548c0dc04732d2105564d33bf90ea1
pranay341 <ro200341@rguktong.ac.in>
2025-10-06 21:42:35 +0530

    not found

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,6 +1,8 @@
 const NotFound = () => {
     return ( 
+        <div style={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
         <h1>Not Found 404</h1>
+        </div>
      );
 }
  
