c5ec43ed9deba6e9a0190c1307eb7db4372f2d99
pranay341 <ro200341@rguktong.ac.in>
2025-10-06 21:39:02 +0530

    not found

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home'
 import Navbar from './components/navbar'  
 import Login from './components/login'
 import Signup from './components/signup'
+import NotFound from "./components/NotFound"
 
 function App() {
   const {user}=useAuthContext();  
@@ -16,7 +17,7 @@ function App() {
     <Route path='/' element={user?<Home/>:<Navigate to='/login'/>}/>
     <Route path='/login' element={!user?<Login/>:<Navigate to='/'/>}/>
     <Route path='/signup' element={!user?<Signup/>:<Navigate to='/'/>}/> 
-    <Route element={<h1>404 Not Found</h1>}/>
+    <Route element={<NotFound/>}/>
   </Routes>
 </BrowserRouter>
   )
