09afdc5fe0e6876eaaa79fadaa309cc7bacaafce
MUPPANENI PRANAY SANKAR <122502110+pranay-sankar-muppanenei@users.noreply.github.com>
2026-09-17 12:46:14 +0530

    Update CORS configuration to allow all origins

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,7 @@ app.use((req,res,next)=>{
 })
 app.use(express.json());    
 app.use(cors({
-  origin: [
-    "https://workout-mate-ocsd.vercel.app"
-  ],
+  origin: true,
   credentials: true
 }));
 
